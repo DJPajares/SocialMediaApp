569759abc547dc33c6c7232c4dd4e5a7e510ddcf
DJPajares <dj.pajares@gmail.com>
2023-05-06 00:35:51 +0800

    feat: add explore screen

---
+++ containers/ExploreScreen.tsx
@@ -0,0 +1,54 @@
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import React from 'react';
+import {Text, SafeAreaView, StyleSheet, View} from 'react-native';
+import Header from '../components/Header';
+
+type RootStackParamList = {
+  Notifications: undefined;
+};
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Notifications'
+>;
+
+type Props = {
+  navigation: HomeScreenNavigationProp;
+};
+
+const ExploreScreen = ({navigation}: Props) => {
+  return (
+    <SafeAreaView style={styles.flex}>
+      <Header />
+
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Explore screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ExploreScreen;
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    marginHorizontal: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 22,
+    fontWeight: '500',
+    marginVertical: 5,
+  },
+  bodyText: {
+    fontSize: 16,
+    fontWeight: '400',
+    color: 'blue',
+    marginVertical: 5,
+  },
+});
