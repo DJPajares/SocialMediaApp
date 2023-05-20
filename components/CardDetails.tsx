571a4153a3938ee984a8ce6d29ee0ba8fe29afa4
DJPajares <dj.pajares@gmail.com>
2023-05-20 21:54:00 +0800

    refactor: add currency formatter and cleanup

--- components/CardDetails.tsx
+++ components/CardDetails.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {formatCurrency} from '../utils/formatUtils';
 
 type CardDetailsProps = {
   name: string;
   price: number;
-  // username: string;
-  // userImage: string;
+  currency: string;
   likeCount: number;
 };
 
-const CardDetails = ({
-  name,
-  price,
-  // username,
-  // userImage,
-  likeCount,
-}: CardDetailsProps) => {
+const CardDetails = ({name, price, currency, likeCount}: CardDetailsProps) => {
   return (
     <View>
       <View style={styles.boxContainer}>
@@ -29,32 +23,16 @@ const CardDetails = ({
           <Text style={styles.likeText}>{likeCount}</Text>
         </View>
 
-        <Text style={styles.priceText}>{`$${price}`}</Text>
-
-        {/* <View style={styles.userContainer}>
-          <Image style={styles.userImage} source={{uri: userImage}} />
-          <Text numberOfLines={1} ellipsizeMode="tail" style={styles.username}>
-            {username}
-          </Text>
-        </View> */}
+        <Text style={styles.priceText}>
+          {formatCurrency({value: price, currency})}
+        </Text>
       </View>
 
       <View style={styles.boxContainer}>
         <Text numberOfLines={1} ellipsizeMode="tail" style={styles.nameText}>
           {name}
         </Text>
-        {/* <Text style={styles.priceText}>{`$${price}`}</Text> */}
       </View>
-
-      {/* TODO: Separate as Avatar component */}
-      {/* <View style={styles.boxContainer}>
-        <View style={styles.userContainer}>
-          <Image style={styles.userImage} source={{uri: userImage}} />
-          <Text numberOfLines={1} ellipsizeMode="tail" style={styles.username}>
-            {username}
-          </Text>
-        </View>
-      </View> */}
     </View>
   );
 };
@@ -75,24 +53,8 @@ const styles = StyleSheet.create({
     fontSize: 12,
     marginLeft: 4,
   },
-  // userContainer: {
-  //   flexDirection: 'row',
-  //   alignItems: 'center',
-  //   // width: '50%',
-  // },
-  // userImage: {
-  //   width: 20,
-  //   height: 20,
-  //   borderRadius: 10,
-  //   marginRight: 4,
-  // },
-  // username: {
-  //   fontSize: 12,
-  //   // width: '75%',
-  // },
   nameText: {
     fontSize: 12,
-    // width: '60%',
   },
   priceText: {
     fontSize: 12,
