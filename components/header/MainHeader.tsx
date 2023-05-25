99cb8531f0c9053e9ab5650e18a8d6b8cc730d6b
DJPajares <dj.pajares@gmail.com>
2023-05-25 09:53:00 +0800

    feat: add dark mode button

--- components/header/MainHeader.tsx
+++ components/header/MainHeader.tsx
@@ -1,29 +1,54 @@
 import {useNavigation} from '@react-navigation/native';
+import {useColorScheme} from 'nativewind';
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const MainHeader = () => {
   const navigation = useNavigation();
 
-  const handleNotifications = () => {
+  const {colorScheme, toggleColorScheme} = useColorScheme();
+  const isDarkMode = colorScheme === 'dark';
+
+  const handleNavigateToNotifications = () => {
     navigation.navigate('Notifications');
   };
 
+  const handleNavigateToHome = () => {
+    navigation.navigate('HomeStack');
+  };
+
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => navigation.navigate('HomeStack')}>
-        <Text style={styles.appNameText}>atrillion</Text>
+    <View className="flex-row justify-between items-center p-4 border-b border-b-divider dark:border-b-divider-dark">
+      <TouchableOpacity onPress={handleNavigateToHome}>
+        <Text className="text-lg font-bold text-primary">atrillion</Text>
       </TouchableOpacity>
 
-      <View style={styles.iconMenu}>
-        <TouchableOpacity
-          style={styles.iconSpacing}
-          onPress={handleNotifications}>
-          <MaterialCommunityIcons name="bell" size={24} color="black" />
+      <View className="flex-row">
+        <TouchableOpacity className="ml-2" onPress={toggleColorScheme}>
+          <MaterialCommunityIcons
+            name={
+              colorScheme === 'dark' ? 'white-balance-sunny' : 'brightness-2'
+            }
+            size={24}
+            color={isDarkMode ? 'white' : 'black'}
+          />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.iconSpacing}>
-          <MaterialCommunityIcons name="cog" size={24} color="black" />
+        <TouchableOpacity
+          className="ml-2"
+          onPress={handleNavigateToNotifications}>
+          <MaterialCommunityIcons
+            name="bell"
+            size={24}
+            color={isDarkMode ? 'white' : 'black'}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity className="ml-2">
+          <MaterialCommunityIcons
+            name="cog"
+            size={24}
+            color={isDarkMode ? 'white' : 'black'}
+          />
         </TouchableOpacity>
       </View>
     </View>
@@ -31,25 +56,3 @@ const MainHeader = () => {
 };
 
 export default MainHeader;
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottomColor: '#ebe8e8',
-    borderBottomWidth: 1,
-  },
-  iconMenu: {
-    flexDirection: 'row',
-  },
-  iconSpacing: {
-    marginLeft: 8,
-  },
-  appNameText: {
-    color: '#2b7bd6',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
